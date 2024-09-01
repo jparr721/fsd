6ed7335296870ad7f49693ae975d04c188e08379
Jarred Parr <jparr721@gmail.com>
2024-09-01 17:10:18 -0600

    database working, need to add delete

--- pkg/tasks/metadata_task.go
+++ pkg/tasks/metadata_task.go
@@ -4,26 +4,35 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"fsd/pkg/ipc"
 	"io/fs"
 	"path/filepath"
+	"time"
 
 	"github.com/fsnotify/fsnotify"
 	_ "github.com/mattn/go-sqlite3"
 	"go.uber.org/zap"
 )
 
-const METADATA_DB string = "fsd.db"
+const METADATA_DB_FILENAME string = "fsd.db"
+
+// METADATA_CREATE creates the metadata database table
 const METADATA_CREATE string = `
 	CREATE TABLE IF NOT EXISTS metadata (
 		id INTEGER NOT NULL PRIMARY KEY,
 		full_path TEXT NOT NULL,
 		size_bytes INTEGER NOT NULL,
+		file_mode INTEGER NOT NULL,
 		is_directory INTEGER NOT NULL,
 		created_at DATETIME NOT NULL,
-		updated_at DATETIME NOT NULL
+		modified_at DATETIME NOT NULL,
+		is_deleted INTEGER NOT NULL DEFAULT 0
 	)
 `
+
+// METADATA_UPDATE_INTERVAL is how often we update the metadata for all of the files
+const METADATA_UPDATE_INTERVAL = 500 * time.Millisecond
 
 type MetadataMessage struct {
 	Name      string `json:"event_name"`
@@ -60,11 +69,12 @@ type MetadataTaskState struct {
 	// watcher is a reference to the fsnotify watcher, which we use to update our file index.
 	watcher *fsnotify.Watcher
 
+	// db is the sqlite database handle
 	db *sql.DB
 }
 
 func NewMetadataTaskState(rootPath string, broadcaster *ipc.Broadcaster, broadcastChannel chan ipc.Message, watcher *fsnotify.Watcher) *MetadataTaskState {
-	db, err := sql.Open("sqlite3", METADATA_DB)
+	db, err := sql.Open("sqlite3", METADATA_DB_FILENAME)
 	if err != nil {
 		zap.L().Fatal("failed to open sqlite database", zap.Error(err))
 	}
@@ -112,12 +122,102 @@ func NewMetadataTask(state *MetadataTaskState) *MetadataTask {
 	}
 }
 
+func (mt *MetadataTask) startMetadataUpdateTask(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			zap.L().Info("got shutdown signal, exiting", zap.String("task name", fmt.Sprintf("%s-%s", MetadataTaskName(), "UpdateTask")))
+			return
+		case <-time.After(METADATA_UPDATE_INTERVAL):
+			zap.L().Debug("updating file metadata")
+			go mt.recursivelyUpdateMetadata(ctx)
+		}
+	}
+}
+
+// recursivelyUpdateMetadata runs as a goroutine and starts from `mt.rootPath` and walks through all
+// files, inserting their characteristics into the sqlite database.
+func (mt *MetadataTask) recursivelyUpdateMetadata(ctx context.Context) {
+	select {
+	case <-ctx.Done():
+		zap.L().Info("got shutdown signal, exiting", zap.String("task name", fmt.Sprintf("%s-%s-%s", MetadataTaskName(), "UpdateTask", "UpdateMetadataOperation")))
+		return
+	case <-time.After(METADATA_UPDATE_INTERVAL):
+		zap.L().Error("metadata update overlap!")
+	case <-time.After(2 * METADATA_UPDATE_INTERVAL):
+		zap.L().Error("still delayed updating, killing task")
+		return
+	default:
+		tx, err := mt.state.db.BeginTx(ctx, nil)
+		if err != nil {
+			zap.L().Error("failed to begin transaction", zap.Error(err))
+			return
+		}
+
+		stmt, err := tx.Prepare(`
+			INSERT INTO metadata (full_path, size_bytes, file_mode, is_directory, created_at, modified_at, is_deleted)
+			VALUES (?, ?, ?, ?, ?, ?, ?)
+		`)
+		if err != nil {
+			tx.Rollback()
+			zap.L().Error("failed to prepare insert statement", zap.Error(err))
+		}
+		defer stmt.Close()
+
+		// Otherwise, begin updating from the walk
+		err = filepath.Walk(mt.state.rootPath, func(path string, info fs.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			// Golang is silly...
+			isDirectory := 0
+			if info.IsDir() {
+				isDirectory = 1
+			}
+
+			_, err = stmt.Exec(
+				path,
+				info.Size(),
+				info.Mode().Perm(),
+				isDirectory,
+				time.Now(),
+				info.ModTime(),
+				0,
+			)
+
+			if err != nil {
+				return err
+			}
+
+			return nil
+		})
+
+		if err != nil {
+			zap.L().Error("metadata update task failed", zap.Error(err))
+		}
+
+		// Commit the transaction
+		if err := tx.Commit(); err != nil {
+			zap.L().Error("failed to commit transaction", zap.Error(err))
+			return
+		}
+
+		zap.L().Info("metadata update successful")
+
+		return
+	}
+}
+
 func (mt *MetadataTask) StartEventLoop(ctx context.Context) {
+	// Start background tasks
+	go mt.startMetadataUpdateTask(ctx)
+
 	for {
 		select {
 		case event := <-mt.state.BroadcastChannel():
 			if err := mt.HandlMessage(event); err != nil {
-				zap.L().Error("Error handling message", zap.String("task name", MetadataTaskName()), zap.Error(err))
+				zap.L().Error("error handling message", zap.String("task name", MetadataTaskName()), zap.Error(err))
 			}
 		case <-ctx.Done():
 			zap.L().Info("got shutdown signal, exiting", zap.String("task name", MetadataTaskName()))
@@ -130,7 +230,7 @@ func (mt *MetadataTask) StartEventLoop(ctx context.Context) {
 func (mt *MetadataTask) HandlMessage(msg ipc.Message) error {
 	ms, err := msg.String()
 	if err != nil {
-		zap.L().Error("Received invalid message", zap.Error(err))
+		zap.L().Error("received invalid message", zap.Error(err))
 		return err
 	}
 	zap.L().Debug("got message", zap.String("task name", MetadataTaskName()), zap.String("msg", ms))
@@ -150,7 +250,7 @@ func (mt *MetadataTask) HandlMessage(msg ipc.Message) error {
 func (mt *MetadataTask) SendMessage(msg ipc.Message) error {
 	ms, err := msg.String()
 	if err != nil {
-		zap.L().Error("Attempted to send an invalid message", zap.Error(err))
+		zap.L().Error("attempted to send an invalid message", zap.Error(err))
 		return err
 	}
 	zap.L().Debug("sent message", zap.String("task name", MetadataTaskName()), zap.String("msg", ms))
@@ -166,7 +266,7 @@ func (mt *MetadataTask) CreateMetadataEntry(name string) error {
 		}
 
 		if info.IsDir() {
-			zap.L().Debug("Adding subdirectory", zap.String("path", path))
+			zap.L().Debug("adding subdirectory", zap.String("path", path))
 			err = mt.state.watcher.Add(path)
 			if err != nil {
 				return err
